5f60260d8147d4df3f702839c0621c9b27828407
João Enrique <130872112+Jedev1@users.noreply.github.com>
2024-05-24 14:58:16 -0300

    update src/js/game.js

---
+++ src/js/game.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementbyId("canvas")
+const canvasctx = canvas.getContext("2d")
+const pacmanFrames = document.getElementbyId("animations")
+const ghostFrames  = document.getElementbyId("ghosts")
+
